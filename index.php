88ec08791badccb6c6c770839008cc80b525f575
MD Jahidul Islam Jiku <safzahidul.cse@gmail.com>
2022-11-24 07:04:04 +0600

    null-value-insert-validation-done

--- index.php
+++ index.php
@@ -61,25 +61,37 @@ include('db.php');
                     $search_value = $_POST["search_value"];
                     $_SESSION["search_value"] = $search_value;
 
-
                     $server_ip =  $_SERVER['SERVER_ADDR'];
                     $_SESSION["server_ip"] = $server_ip;
 
                     $date = date('m-d-Y h:i:s', time());
                     $_SESSION["date"] = $date;
 
-                    $sql = "INSERT INTO search_information (search_term, ip_address, date_time) VALUES ('{$_SESSION['search_value']}','{$_SESSION['server_ip']}','{$_SESSION['date']}')";
+                    if ($_SESSION["search_value"] != null) {
+                        $sql = "INSERT INTO search_information (search_term, ip_address, date_time) VALUES ('{$_SESSION['search_value']}','{$_SESSION['server_ip']}','{$_SESSION['date']}')";
 
-                    if (mysqli_query($mysqli, $sql)) {
-                        echo "New record created successfully";
-                        session_destroy();
-                        header("index.php");
+                        if (mysqli_query($mysqli, $sql)) {
+                            echo $_SESSION['search_value'] . " Search Found successfully";
+                            // Finally, destroy the session.
+                            session_destroy();
+                            header("index.php");
+                        } else {
+                            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                        }
                     } else {
-                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                        echo "No Result Found. Please enter value";
+                        // default fetch api
+                        $page = rand(1, 99);
+                        $json = "https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=$page";
+                        $json = preg_replace('/\s+/', '%', $json);
+                        $json = file_get_contents($json);
+                        $obj = json_decode($json);
+                        $result = array();
                     }
                 } else {
                     // default fetch api
-                    $json = 'https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=movie';
+                    $page = rand(1, 99);
+                    $json = "https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=$page";
                     $json = preg_replace('/\s+/', '%', $json);
                     $json = file_get_contents($json);
                     $obj = json_decode($json);
